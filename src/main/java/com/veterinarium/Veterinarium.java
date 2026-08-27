87993c8b3cf7fc9d73133ace40989dc6476e739a
Veterinarium <alexis@veterinarium.dev>
2026-08-27 12:38:55 +0200

    fix: livre recettes visible sans taper @veterinarium (parent minecraft:recipes/root) - 1.9.5 + sync Max

--- src/main/java/com/veterinarium/Veterinarium.java
+++ src/main/java/com/veterinarium/Veterinarium.java
@@ -40,6 +40,6 @@ public class Veterinarium {
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-        LOGGER.info("HELLO FROM VETERINARIUM COMMON SETUP - Bloc Opératoire prêt !........."); // 1.9.4 recettes visibles dès début
+        LOGGER.info("HELLO FROM VETERINARIUM COMMON SETUP - Bloc Opératoire prêt !.........."); // 1.9.5 fix livre recettes parent minecraft
     }
 }
